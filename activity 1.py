fb6c64d44a371475bee32da3129691ddbf2e4c6b
CODERPARTHPATEL <nskp.patel09@gmail.com>
2025-11-01 12:45:05 +0000

    lesson 29

---
+++ activity 1.py
@@ -0,0 +1,19 @@
+#print a senace
+print("hello everyone")
+
+#print number
+print(4)
+
+#print both
+print("the number being printed is",4)
+
+#print empty line before/;after a sentance
+print("\n i am parth")
+
+#performing and printing a simple caculation
+print("hte result of 3 + 5 is:",3 + 5)
+
+#print all the keywords present in python
+print("keywords in python are...")
+import keyword
+print(keyword.kwlist)
